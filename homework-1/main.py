cf125a631a402eb6339bda5d10bc791df803eb33
Andrey Saksonov <asaksonov@gmail.com>
2023-09-18 09:50:00 +0300

    Реализована функция заполнения БД из CSV файла Прописан скрипт создания таблиц в БД

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,42 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+csv_file_customers = "north_data/customers_data.csv"
+csv_file_employees = "north_data/employees_data.csv"
+csv_file_orders = "north_data/orders_data.csv"
+
+
+# connect to db
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12345"
+)
+
+
+# create cursor
+cur = conn.cursor()
+
+
+# create functions writing data
+def writing_data_to_table(filename, table_name):
+    with open(filename) as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            quantity_el = '%s, ' * len(row)
+            cur.execute(f"INSERT INTO {table_name} VALUES({quantity_el[:-2]})", (row[::]))
+
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            writing_data_to_table(csv_file_customers, 'customers')
+
+            writing_data_to_table(csv_file_employees, 'employees')
+
+            writing_data_to_table(csv_file_orders, 'orders')
+finally:
+    conn.close()
